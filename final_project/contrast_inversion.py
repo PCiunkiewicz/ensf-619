1dfee9428e4aa7486e7809318e7e02b809d34ae1
Philip Ciunkiewicz <philip.ciunkiewicz@ucalgary.ca>
2023-03-18 01:19:46 -0600

    Set up paths and contrast inversion

---
+++ final_project/contrast_inversion.py
@@ -0,0 +1,34 @@
+import os
+
+import tqdm
+import nibabel as nib
+
+from paths import ORIGINALS, NEGATIVES, MASKS
+
+
+def contrast_invert(filename, save=False):
+    """
+    Perform contrast inversion on gray and white matter
+    in the brain images. Optionally save as nifti.
+    """
+    img = nib.load(ORIGINALS / filename)
+    affine = img.affine
+    img = img.get_fdata()
+
+    mask = nib.load(MASKS / filename).get_fdata()
+    mask = (mask == 2) | (mask == 3)
+
+    negative = img * mask
+    negative = (negative.max() - negative) * mask
+    negative += img * ~mask
+
+    if save:
+        nii = nib.Nifti1Image(negative, affine)
+        nib.save(nii, NEGATIVES / filename)
+
+    return negative
+
+
+if __name__ == '__main__':
+    for filename in tqdm.tqdm(os.listdir(ORIGINALS)):
+        contrast_invert(filename, save=True)
---
+++ final_project/paths.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+
+DATA_PATH = Path(__file__).parent.resolve() / 'dataset'
+ORIGINALS = DATA_PATH / 'adult/original'
+NEGATIVES = DATA_PATH / 'adult/negative'
+MASKS = DATA_PATH / 'adult/mask'
+NEWBORNS = DATA_PATH / 'newborn/original'
