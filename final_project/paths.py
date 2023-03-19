51799c1d0c4395dbcdd2a2c5f3d5904600054ee4
Philip Ciunkiewicz <philip.ciunkiewicz@ucalgary.ca>
2023-03-18 18:02:18 -0600

    Add model paths

--- final_project/paths.py
+++ final_project/paths.py
@@ -6,3 +6,6 @@ ORIGINALS = DATA_PATH / 'adult/original'
 NEGATIVES = DATA_PATH / 'adult/negative'
 MASKS = DATA_PATH / 'adult/mask'
 NEWBORNS = DATA_PATH / 'newborn/original'
+
+MODEL_PATH = Path(__file__).parent.resolve() / 'models'
+PRETRAINED_PATH = MODEL_PATH / 'pretrained'
