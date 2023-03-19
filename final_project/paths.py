bf02429acf725379a34e94f06bd84db24b9726d6
Philip Ciunkiewicz <philip.ciunkiewicz@ucalgary.ca>
2023-03-18 18:17:02 -0600

    Add colab filesystem support

--- final_project/paths.py
+++ final_project/paths.py
@@ -1,11 +1,17 @@
+import os
 from pathlib import Path
 
 
-DATA_PATH = Path(__file__).parent.resolve() / 'dataset'
+if 'COLAB_GPU' in os.environ:
+    ROOT = Path('/content/drive/MyDrive/ENSF619')
+else:
+    ROOT = Path(__file__).parent.resolve()
+
+DATA_PATH = ROOT / 'dataset'
 ORIGINALS = DATA_PATH / 'adult/original'
 NEGATIVES = DATA_PATH / 'adult/negative'
 MASKS = DATA_PATH / 'adult/mask'
 NEWBORNS = DATA_PATH / 'newborn/original'
 
-MODEL_PATH = Path(__file__).parent.resolve() / 'models'
+MODEL_PATH = ROOT / 'models'
 PRETRAINED_PATH = MODEL_PATH / 'pretrained'
