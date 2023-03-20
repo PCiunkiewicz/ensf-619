1c44f347d32b9531950e1571e76fb5d35def84e6
Philip Ciunkiewicz <philip.ciunkiewicz@ucalgary.ca>
2023-03-19 18:37:02 -0600

    Move functions to utils, create img transforms

--- final_project/transform.py
+++ final_project/transform.py
@@ -1,54 +1,17 @@
-import numpy as np
+from torchvision import transforms
 
 
-def fft_transform(x, to='kspace'):
+class DeepCascadeTransform:
     """
-    Transform a 2d image from image-space to k-space,
-    or transform from k-space to image-space.
+    Dataset transformation utility class
     """
-    assert to in {'kspace', 'image'}, 'kwarg `to` must be one of "kspace" | "image"'
-    if to == 'kspace':
-        return np.fft.fft2(x)
-    elif to == 'image':
-        return np.fft.ifft2(x)
+    def __init__(self, size):
+        self.pipeline = transforms.Compose([
+            transforms.RandomRotation(degrees=20),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.RandomResizedCrop(size=(size, size), scale=(0.95, 1))
+        ])
 
-
-def minmax(x):
-    """
-    Perform min-max normalization to input data.
-    """
-    return  (x - x.min()) / (x.max() - x.min())
-
-
-def remove_blank_slices(x):
-    """
-    Remove blank slices from the input image.
-    """
-    mask = x.sum(axis=(0, 1)) != 0
-    return x[:,:,mask]
-
-
-def center_crop_2d(x, size):
-    """
-    Center crop a 2d image.
-    """
-    assert len(size) == 2, 'size must be a tuple of length 2'
-    h, w = x.shape[:2]
-    ch, cw = size
-    assert ch <= h and cw <= w, 'size must be smaller than image'
-    h1 = (h - ch) // 2
-    w1 = (w - cw) // 2
-    return x[h1:h1+ch, w1:w1+cw]
-
-
-def zero_pad_2d(x, size):
-    """
-    Zero pad a 2d image.
-    """
-    assert len(size) == 2, 'size must be a tuple of length 2'
-    h, w = x.shape[:2]
-    ch, cw = size
-    assert ch >= h and cw >= w, 'size must be larger than image'
-    h1 = (ch - h) // 2
-    w1 = (cw - w) // 2
-    return np.pad(x, ((h1, ch-h-h1), (w1, cw-w-w1)), 'constant')
+    def __call__(self, img):
+        return self.pipeline(img)
---
+++ final_project/utils.py
@@ -0,0 +1,62 @@
+import torch
+import numpy as np
+
+
+def fft_transform(x, to='kspace'):
+    """
+    Transform a 2d image from image-space to k-space,
+    or transform from k-space to image-space.
+    """
+    assert to in {'kspace', 'image'}, 'kwarg `to` must be one of "kspace" | "image"'
+    if to == 'kspace':
+        return np.fft.fft2(x)
+    elif to == 'image':
+        return np.fft.ifft2(x)
+
+
+def nrmse(preds, target):
+    """
+    Calculate normalized root mean squared error in PyTorch.
+    """
+    return torch.sqrt(torch.mean((preds - target)**2)) / torch.sqrt(torch.mean(target**2))
+
+
+def minmax(x):
+    """
+    Perform min-max normalization to input data.
+    """
+    return  (x - x.min()) / (x.max() - x.min())
+
+
+def remove_blank_slices(x):
+    """
+    Remove blank slices from the input image.
+    """
+    mask = x.sum(axis=(0, 1)) != 0
+    return x[:,:,mask]
+
+
+def center_crop_2d(x, size):
+    """
+    Center crop a 2d image.
+    """
+    assert len(size) == 2, 'size must be a tuple of length 2'
+    h, w = x.shape[:2]
+    ch, cw = size
+    assert ch <= h and cw <= w, 'size must be smaller than image'
+    h1 = (h - ch) // 2
+    w1 = (w - cw) // 2
+    return x[h1:h1+ch, w1:w1+cw]
+
+
+def zero_pad_2d(x, size):
+    """
+    Zero pad a 2d image.
+    """
+    assert len(size) == 2, 'size must be a tuple of length 2'
+    h, w = x.shape[:2]
+    ch, cw = size
+    assert ch >= h and cw >= w, 'size must be larger than image'
+    h1 = (ch - h) // 2
+    w1 = (cw - w) // 2
+    return np.pad(x, ((h1, ch-h-h1), (w1, cw-w-w1)), 'constant')
